8ab71f4f03eb1d003ebb38a5e6884fddbfe8e070
Jason Cassella <jasoncassella1@gmail.com>
2022-12-03 12:06:03 -0500

    Built Hero component

---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+	return (
+		<section className='hero'>
+			<img src='/src/assets/photo-grid.png' width='395.91' />
+			<div>
+				<h1>Online Experiences</h1>
+				<p>
+					Join unique interactive activities led by one-of-a-kind hosts—all without
+					leaving home.
+				</p>
+			</div>
+		</section>
+	);
+}
