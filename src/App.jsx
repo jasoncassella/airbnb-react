ed371be1e67fd32afebbb9401653d48c808e08d3
Jason Cassella <jasoncassella1@gmail.com>
2022-12-09 19:07:43 -0500

    hid the hero element and added data

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
 
-function App() {
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
 	return (
 		<div className='app'>
 			<Navbar />
-			<Hero />
+			{/* <Hero /> */}
 			<Card
 				img='katie-zaferes.png'
 				rating='5.0'
 				reviewCount={6}
-				country='USA'
+				location='USA'
 				title='Life Lessons with Katie Zaferes'
 				price={136}
 			/>
 		</div>
 	);
 }
-
-export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,13 +2,12 @@ export default function Card(props) {
 	return (
 		<div className='card'>
 			<img src={`/src/assets/${props.img}`} width='176' />
-			<div className='image-label'>sold out</div>
 			<div className='line-1'>
 				<img src='/src/assets/star.png' width='14' />
 				<span>{props.rating}</span>
 				<span className='gray'>({props.reviewCount})</span>
 				<img src='/src/assets/Ellipse-6.png' width='2' />
-				<span className='gray'>{props.country}</span>
+				<span className='gray'>{props.location}</span>
 			</div>
 			<div className='line-2'>
 				<span>{props.title}</span>
